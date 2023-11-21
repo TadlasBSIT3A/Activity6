7576ed9750df5c65348b2d22af69b7961c8352dd
TadlasBSIT3A <143198653+TadlasBSIT3A@users.noreply.github.com>
2023-11-21 20:10:13 +0800

    Add files via upload

---
+++ src/Clients.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+export const Clients = [
+
+    {
+        id: 1,
+        name:{
+            firstname: "Kenneth",
+            lastname: "Tadlas",
+        },
+        course:"BSIT",
+        yearsection: "3A",
+        username: "tads",
+        password: "123",
+        usertype: "Student",
+    },
+    {
+        id: 2,
+        name:{
+            firstname: "Lemark",
+            lastname: "Limocon",
+        },
+        course:"BSCS",
+        yearsection: "4B",
+        username: "marko", 
+        password: "neen43",
+        usertype: "Student",
+    },
+    {
+        id: 3,
+        name:{
+            firstname: "Rhea",
+            lastname: "Banoc",
+        },
+        course:"BSCRIM",
+        yearsection: "1C",
+        username: "rheaaah",
+        password: "12345",
+        usertype: "Student",
+    },
+    {
+        id: 4,
+        name:{
+            firstname: "Jerry",
+            lastname: "Ale",
+        },
+        course:"",
+        yearsection: "",
+        username: "jeer",
+        password: "jeeralee",
+        usertype: "Teacher",
+    },
+    {
+        id: 5,
+        name:{
+            firstname: "Winzeal",
+            lastname: "Agoc",
+        },
+        course:"",
+        yearsection: "",
+        username: "winwin",
+        password: "zeal123",
+        usertype: "Teacher",
+    },
+    
+];
